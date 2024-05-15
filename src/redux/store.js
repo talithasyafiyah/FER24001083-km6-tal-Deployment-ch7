685f40ebc18cbbc6e77912009c2e8258c4dc0c63
talitha <talithasyafiyah1112@gmail.com>
2024-05-15 19:37:11 +0700

    fix: fix redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,37 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import recipeReducers from "./reducers/recipeReducers";
-import { thunk } from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import authReducers from "./reducers/authReducers";
+import recipeReducers from "./reducers/recipeReducers";
+import { createTransform } from "redux-persist";
 
+const favoritesTransform = createTransform(
+  (inboundState) => inboundState,
+  (outboundState) => outboundState,
+  { whitelist: ["recipe"] }
+);
+
+// Combine reducers
 const rootReducer = combineReducers({
   auth: authReducers,
   recipe: recipeReducers,
 });
 
+// Configure redux-persist
 const persistConfig = {
   key: "root",
   storage,
-  // blacklist: ['recipe'],
+  transforms: [favoritesTransform],
 };
 
+// Create persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Configure the Redux store
 export const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV === "development",
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(thunk),
 });
 
+// Create the Redux persistor
 export const persistor = persistStore(store);
